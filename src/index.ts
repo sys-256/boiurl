005d22898372c7a97a8d7b6193262873eed35d45
Bram <84628826+sys-256@users.noreply.github.com>
2022-04-18 13:45:13 +0200

    send link based on request + optimize sql

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import express from "express";
 const app = express();
 import sqlite3 from "better-sqlite3";
-const database = new sqlite3("./main.sqlite3");
+const database = new sqlite3(`./main.sqlite3`);
 
 const config = {
-    protocol: "http",
-    host: "localhost",
-    port: 3000,
+    port: 8035,
     urlRegex: /^(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?$/,
 };
 
@@ -18,9 +16,9 @@ database.prepare(`CREATE TABLE IF NOT EXISTS url (
     origin TEXT
 );`).run();
 
-app.get("/", (request, response) => {
-    response.status(200).send(
-`<fieldset>
+app.get(`/`, (request, response) => {
+    response.status(200).send(`<!DOCTYPE html>
+<fieldset>
     <legend>URL to shorten</legend>
     <form method="post">
         <input type="url" name="url" required><br><br>
@@ -29,9 +27,9 @@ app.get("/", (request, response) => {
 </fieldset>`);
 });
 
-app.get("/:id", (request, repsonse) => {
+app.get(`/:id`, (request, repsonse) => {
     const id = request.params.id;
-    const query = database.prepare(`SELECT origin FROM url WHERE ID=?`).all(id);
+    const query = database.prepare(`SELECT origin FROM url WHERE ID=?;`).all(id);
     if (query.length > 0) {
         repsonse.status(301).redirect(query[0].origin);
     } else {
@@ -39,8 +37,8 @@ app.get("/:id", (request, repsonse) => {
     }
 });
 
-app.post("/", (request, response) => {
-    // Check if it's a valid url
+app.post(`/`, (request, response) => {
+    // Check if it is a valid url
     const { url } = request.body;
     if (!url || !config.urlRegex.test(url)) {
         response.status(400).send(`Please specify a (valid) URL.`);
@@ -50,19 +48,27 @@ app.post("/", (request, response) => {
     // Check if the url is already in the database
     const query = database.prepare(`SELECT ID FROM url WHERE origin=?;`).all(url);
     if (query.length > 0) {
-        response.status(200).send(`<a href="${config.protocol}://${config.host}:${config.port}/${query[0].ID}">${config.protocol}://${config.host}:${config.port}/${query[0].ID}</a>`);
+        response
+            .status(200)
+            .send(
+                `<a href="//${request.headers.host}/${query[0].ID}">${request.protocol}://${request.headers.host}/${query[0].ID}</a>`,
+            );
         return;
     }
 
     // Generate a random id
-    let id = "";
+    let id = ``;
     for (let i = 0; i < 6; i++) id += Math.floor(Math.random() * 36).toString(36);
 
     // Insert the url into the database and send link
-    database.prepare(`INSERT INTO url (ID, origin) VALUES (?, ?);`).run(id, url);
-    response.status(200).send(`<a href="${config.protocol}://${config.host}:${config.port}/${id}">${config.protocol}://${config.host}:${config.port}/${id}</a>`);
+    database.prepare(`INSERT INTO url VALUES (?, ?);`).run(id, url);
+    response
+        .status(200)
+        .send(
+            `<a href="//${request.headers.host}/${id}">${request.protocol}://${request.headers.host}/${id}</a>`,
+        );
 });
 
 app.listen(config.port, () => {
-    console.log(`Server started on ${config.protocol}://${config.host}:${config.port}.`);
+    console.log(`Server started on port ${config.port}.`);
 });
